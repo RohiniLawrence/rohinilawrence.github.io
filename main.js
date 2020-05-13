243699cbfa8bc326273939e5b099a04b7a5afe64
RohiniLawrence <rohinilawrence@gmail.com>
2020-05-13 19:02:49 -0400

    projects tab menu

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-$(document).ready(function(){
+function showProject(event, language) {
+    var i, langContainer, tablinks;
+    langContainer = document.getElementsByClassName("langcontainer");
+    for (i = 0; i < langContainer.length; i++) {
+      langContainer[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(language).style.display = "block";
+
+
+        event.currentTarget.className.replace(" active", "");
+   
+    AOS.refreshHard(); 
+   
+  }
+
+  $(document).ready(function(){
+    
     $('.menu-toggler').on('click', function(){
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
@@ -13,6 +35,8 @@ $(document).ready(function(){
         }, 2000);
     });
     
+
+
     $('#up').on('click', function(){
         $('html,body').animate({
             scrollTop: 0},2000);
@@ -22,6 +46,9 @@ $(document).ready(function(){
         easing:'ease',
         duration:1300,
          once:true
-    })
+    });
+
+    
+   
   
 });
