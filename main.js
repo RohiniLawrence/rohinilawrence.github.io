a2248d19ac044f66dd6ee4dfa74f24ebcb5748f0
RohiniLawrence <rohinilawrence@gmail.com>
2020-05-22 17:15:12 -0400

    tabs

--- main.js
+++ main.js
@@ -1,31 +1,86 @@
+let tablink;
 function showProject(event, language) {
     var i, langContainer, tablinks;
-    langContainer = document.getElementsByClassName("langcontainer");
-    for (i = 0; i < langContainer.length; i++) {
-      langContainer[i].style.display = "none";
-    }
+    // langContainer = document.getElementsByClassName("langcontainer");
+    // for (i = 0; i < langContainer.length; i++) {
+    //     langContainer[i].style.display = "none";
+    // }
 
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    document.getElementById(language).style.display = "block";
-
+    // document.getElementById(language).style.display = "block";
    
+    $('html, body').animate({
+        scrollTop: $("#"+language).offset().top-100
+    }, 700);
+
     event.currentTarget.className += " active";
-    AOS.refreshHard(); 
-   
-  }
+    AOS.refreshHard();
 
-  
+}
 
-  $(document).ready(function(){
+//   $(function(){
 
-    const parallax= document.querySelector(".landing-text h1");
-    const mountains= document.querySelector("ice-mountains");
 
-   
+
+
+// })
+
+
+//     // console.log("and",androidOffset);
+//     // console.log("cpp",cppOffset);
+//   })
+
+
+$(document).ready(function () {
+
+    window.addEventListener("scroll", function () {
+        let scroll = window.pageYOffset;
+
+        let htmlOffset = $('#html').offset().top;
+        let androidOffset = $('#android').offset().top;
+        let cppOffset = $('#cpp').offset().top;
+        let reactOffset = $('#react').offset().top;
+        let jsOffset = $('#js').offset().top;
+
+        var active;
+        if (scroll > htmlOffset-300) {
+            active = "Html";
+        }
+        if (scroll > androidOffset-300) {
+
+            active = "Android";
+        }
+        if (scroll > cppOffset-300) {
+            active = "Cpp";
+
+        }
+        if (scroll > reactOffset-300) {
+
+            active = "React";
+        }
+        if (scroll > jsOffset-300) {
+            active = "Js";
+
+        }
+if(active!=null){
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+
+        document.querySelector("#btn"+active).className += " active";
+    }
+        // console.log( $("#btn"+active));
+
+    })
+    const parallax = document.querySelector(".landing-text h1");
+    const mountains = document.querySelector("ice-mountains");
+
+
     // window.addEventListener("scroll", function()
     // {
     //     let offset = window.pageYOffset;
@@ -33,38 +88,39 @@ function showProject(event, language) {
     //     // mountains.style.backgroundPositionY = offset  * 10000 +"px";
 
     // })
-  
-    
-    $('.menu-toggler').on('click', function(){
+
+
+    $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
-        
+
         $('.top-nav').toggleClass('open');
-      
+
     });
-    $('.top-nav .nav-link').on('click', function(){
+    $('.top-nav .nav-link').on('click', function () {
         $('.menu-toggler').removeClass('open');
         $('.top-nav').removeClass('open');
     });
-    $('nav a[href*="#"]').on('click', function(){
+    $('nav a[href*="#"]').on('click', function () {
         $('html,body').animate({
-            scrollTop: $($(this).attr('href')).offset().top - 100 
+            scrollTop: $($(this).attr('href')).offset().top - 100
         }, 2000);
     });
-    
 
 
-    $('#up').on('click', function(){
+
+    $('#up').on('click', function () {
         $('html,body').animate({
-            scrollTop: 0},2000);
+            scrollTop: 0
+        }, 2000);
     });
 
     AOS.init({
-        easing:'ease',
-        duration:1300,
-         once:true
+        easing: 'ease',
+        duration: 1300,
+        once: true
     });
 
-    
-   
-  
+
+
+
 });
